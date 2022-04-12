305689527c68043b1143aa49c9dec47a876dd2fb
Maaax02 <massiown@gmail.com>
2022-04-12 12:21:07 +0200

    adds seeder for doctor table

--- app/Doctor.php
+++ app/Doctor.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    //
+    protected $fillable = [
+    'name' ,
+    'surname', 
+    'email' ,
+    'address',
+    'city' ,
+    'password',
+    'photo' ,
+    'number',
+    'curriculum' ,
+    'medicalService'
+    ];
 }
--- database/seeds/DoctorSeeder.php
+++ database/seeds/DoctorSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Doctor;
+use Faker\Generator as Faker;
+
 
 class DoctorSeeder extends Seeder
 {
@@ -9,8 +12,61 @@ class DoctorSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+
+        $doctors = [
+            [
+                'name' => 'Mario',
+                'surname' => 'Rossi',
+                'email' => 'mario.rossi@gmail.com',
+                'address' => $faker->streetAddress(),
+                'city' => $faker->city(),
+                'password' => 'password',
+                'photo' => 'https://www.google.com/search?q=dottore&client=firefox-b-d&sxsrf=APq-WBt5lweiIrVTjPdZyHugfJMRWGNi5A:1649757251456&source=lnms&tbm=isch&sa=X&ved=2ahUKEwip5MDXoI73AhVNS_EDHf4GAFAQ_AUoAXoECAIQAw&biw=1120&bih=925&dpr=1#imgrc=VHr_pUlTtEJXzM',
+                'number' => $faker->phoneNumber(),
+                'curriculum' => $faker->text(),
+                'medicalService' => 'Visita Oculistica'
+            ],
+            [
+                'name' => 'Mario',
+                'surname' => 'verdi',
+                'email' => 'mario.verdi@gmail.com',
+                'address' => $faker->streetAddress(),
+                'city' => $faker->city(),
+                'password' => 'password',
+                'photo' => 'https://www.google.com/search?q=dottore&client=firefox-b-d&sxsrf=APq-WBt5lweiIrVTjPdZyHugfJMRWGNi5A:1649757251456&source=lnms&tbm=isch&sa=X&ved=2ahUKEwip5MDXoI73AhVNS_EDHf4GAFAQ_AUoAXoECAIQAw&biw=1120&bih=925&dpr=1#imgrc=VHr_pUlTtEJXzM',
+                'number' => $faker->phoneNumber(),
+                'curriculum' => $faker->text(),
+                'medicalService' => 'Visita Oncologica'
+            ],
+            [
+                'name' => 'Marco',
+                'surname' => 'Gialli',
+                'email' => 'marco.gialli@gmail.com',
+                'address' => $faker->streetAddress(),
+                'city' => $faker->city(),
+                'password' => 'password',
+                'photo' => 'https://www.google.com/search?q=dottore&client=firefox-b-d&sxsrf=APq-WBt5lweiIrVTjPdZyHugfJMRWGNi5A:1649757251456&source=lnms&tbm=isch&sa=X&ved=2ahUKEwip5MDXoI73AhVNS_EDHf4GAFAQ_AUoAXoECAIQAw&biw=1120&bih=925&dpr=1#imgrc=VHr_pUlTtEJXzM',
+                'number' => $faker->phoneNumber(),
+                'curriculum' => $faker->text(),
+                'medicalService' => 'Visita Pediatrica'
+            ]
+        ];
+        foreach ($doctors as $info ) {
+            $doctors = new Doctor();
+            // $doctors->name =
+            // $doctors->surname =
+            // $doctors->email =
+            // $doctors->address =
+            // $doctors->city =
+            // $doctors->password =
+            // $doctors->photo =
+            // $doctors->number =
+            // $doctors->curriculum =
+            // $doctors->medicalService =
+            $doctors->fill($info);
+            $doctors->save();
+        }
     }
 }
