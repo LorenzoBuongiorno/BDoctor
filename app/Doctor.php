bc1ff3a616dbd68d3ae530f2962fd56b2f606f4b
agres <ale.agresta46@gmail.com>
2022-04-12 12:22:26 +0200

    create specialization table

--- app/Doctor.php
+++ app/Doctor.php
@@ -44,4 +44,8 @@ class Doctor extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function specialzation(){
+        return $this->belongsToMany('App\Specialization');
+    }
 }
