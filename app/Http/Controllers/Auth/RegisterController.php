bf2e1468820636360eaae3674db893afaf1ead3c
AgoDe <ago.defelice@gmail.com>
2022-04-13 11:53:05 +0200

    fix Registration feature

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,9 +6,17 @@ use App\Specialization;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Doctor;
-use Illuminate\Foundation\Auth\RegistersUsers;
+// use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+
+/* usati da RegistersUsers */
+use Illuminate\Foundation\Auth\RedirectsUsers;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+/* end of usati da RegistersUsers */
 
 class RegisterController extends Controller
 {
@@ -23,7 +31,70 @@ class RegisterController extends Controller
     |
     */
 
-    use RegistersUsers;
+    // use RegistersUsers;
+
+     /* ----- RegisterUser Trait ------- */
+
+     use RedirectsUsers;
+
+     /**
+      * Show the application registration form.
+      *
+      * @return \Illuminate\View\View
+      */
+     public function showRegistrationForm()
+     {
+        $type = Specialization::all();
+        return view('auth.register', compact('type'));
+         
+     }
+ 
+     /**
+      * Handle a registration request for the application.
+      *
+      * @param  \Illuminate\Http\Request  $request
+      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+      */
+     public function register(Request $request)
+     {
+         $this->validator($request->all())->validate();
+ 
+         event(new Registered($user = $this->create($request->all())));
+ 
+         $this->guard()->login($user);
+ 
+         if ($response = $this->registered($request, $user)) {
+             return $response;
+         }
+ 
+         return $request->wantsJson()
+                     ? new JsonResponse([], 201)
+                     : redirect($this->redirectPath());
+     }
+ 
+     /**
+      * Get the guard to be used during registration.
+      *
+      * @return \Illuminate\Contracts\Auth\StatefulGuard
+      */
+     protected function guard()
+     {
+         return Auth::guard();
+     }
+ 
+     /**
+      * The user has been registered.
+      *
+      * @param  \Illuminate\Http\Request  $request
+      * @param  mixed  $user
+      * @return mixed
+      */
+     protected function registered(Request $request, $user)
+     {
+         //
+     }
+ 
+     /* ----- end of RegisterUser Trait ------- */
 
     /**
      * Where to redirect users after registration.
@@ -85,9 +156,5 @@ class RegisterController extends Controller
      *
      * @return \Illuminate\View\View
      */
-    public function showRegistrationForm()
-    {
-        $type = Specialization::all();
-        return view('auth.register', compact('type'));
-    }
+   
 }
