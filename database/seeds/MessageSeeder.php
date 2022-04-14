68fe9f616867a67aba5d08c046f69296941e43d2
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-04-14 17:45:17 +0200

    create messages and reviews seeder

--- database/seeds/MessageSeeder.php
+++ database/seeds/MessageSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Doctor;
+use App\Message;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class MessageSeeder extends Seeder
 {
@@ -9,8 +12,23 @@ class MessageSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker, Doctor $doctor)
     {
-        //
+       // create an array with all doctor.id
+        $doctors = Doctor::all()->pluck('id')->toArray();
+
+        for ($i = 0; $i<=10; $i++) {
+
+            $message = new Message();
+            
+            $message->name = $faker->name();
+            $message->email = $faker->email();
+            $message->text = $faker->realText();
+            $message->doctor_id = $faker->randomElement($doctors);
+            $message->created_at = $faker->dateTime($max = 'now');
+            $message->updated_at = $faker->dateTime($max = 'now');
+            
+            $message->save();
+        }
     }
 }
--- database/seeds/ReviewSeeder.php
+++ database/seeds/ReviewSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Doctor;
+use App\Review;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ReviewSeeder extends Seeder
 {
@@ -9,8 +12,23 @@ class ReviewSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker, Doctor $doctor)
     {
-        //
+        // create an array with all doctor.id
+        $doctors = Doctor::all()->pluck('id')->toArray();
+
+        for ($i = 0; $i<=10; $i++) {
+
+            $review = new Review();
+
+            $review->created_at = $faker->dateTime();
+            $review->updated_at = $faker->dateTime();
+            $review->doctor_id = $faker->randomElement($doctors);
+            $review->name = $faker->name();
+            $review->text = $faker->realText();
+            $review->vote = $faker->numberBetween(0, 5);
+
+            $review->save();
+        }
     }
 }
