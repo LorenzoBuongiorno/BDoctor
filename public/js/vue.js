95948d3720e6ddb8043b267ba5fa0b3fcb41cfea
Maaax02 <massiown@gmail.com>
2022-04-27 10:34:41 +0200

    Create first modal

--- public/js/vue.js
+++ public/js/vue.js
@@ -2042,6 +2042,39 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: {
@@ -2050,10 +2083,10 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
   data: function data() {
     return {
       newMessage: {
-        name: '',
-        email: '',
-        text: '',
-        doctor_id: ''
+        name: "",
+        email: "",
+        text: "",
+        doctor_id: ""
       }
     };
   },
@@ -2067,7 +2100,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
               case 0:
                 this.newMessage.doctor_id = this.doctor_id;
                 _context.next = 3;
-                return axios__WEBPACK_IMPORTED_MODULE_1___default.a.post('api/message', this.newMessage);
+                return axios__WEBPACK_IMPORTED_MODULE_1___default.a.post("api/message", this.newMessage);
 
               case 3:
               case "end":
@@ -2082,7 +2115,13 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
       }
 
       return postMessage;
-    }()
+    }(),
+    openModal: function openModal() {
+      document.querySelector(".bg-modal").style.display = "flex";
+    },
+    closeModal: function closeModal() {
+      document.querySelector(".bg-modal").style.display = "none";
+    }
   }
 });
 
@@ -2557,6 +2596,25 @@ exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loa
 
 // module
 exports.push([module.i, ".home-container .btn.orange[data-v-eca07cf4] {\n  background-color: #ff6700;\n  color: whitesmoke;\n}\n.home-container nav[data-v-eca07cf4] {\n  height: 80px;\n  background-color: #3a6ea5;\n}\n.home-container .jumbotron[data-v-eca07cf4] {\n  height: 91.5vh;\n  background-image: url(\"https://www.juniordoctors.eu/themes/custom/ejd/images/ejd-home-header.jpg\");\n  background-size: cover;\n  background-position: center;\n}\n.home-container .jumbotron .text-search[data-v-eca07cf4] {\n  height: 200px;\n  border-radius: 20px;\n  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);\n  background: rgba(0, 0, 0, 0.274);\n}\n.home-container .jumbotron .text-search input[data-v-eca07cf4] {\n  border: 2px solid orange;\n}", ""]);
+
+// exports
+
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/sendMessage.vue?vue&type=style&index=0&id=4d2a3ae4&lang=scss&scoped=true&":
+/*!*********************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--7-2!./node_modules/sass-loader/dist/cjs.js??ref--7-3!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/sendMessage.vue?vue&type=style&index=0&id=4d2a3ae4&lang=scss&scoped=true& ***!
+  \*********************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".message[data-v-4d2a3ae4] {\n  position: relative;\n}\n.bg-modal[data-v-4d2a3ae4] {\n  width: 100%;\n  height: 100%;\n  position: absolute;\n  top: 0;\n  display: none;\n  justify-content: center;\n  align-items: center;\n}\n.modal-contents[data-v-4d2a3ae4] {\n  height: 100%;\n  width: 500px;\n  background-color: white;\n  text-align: center;\n  padding: 20px;\n  position: relative;\n  border-radius: 4px;\n  background-color: #3A6EA5;\n  overflow: auto;\n}\n.close[data-v-4d2a3ae4] {\n  width: 190px;\n  margin: 50px auto;\n  bottom: 0;\n  font-size: 14px;\n}", ""]);
 
 // exports
 
@@ -3925,6 +3983,36 @@ if(false) {}
 
 /***/ }),
 
+/***/ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/sendMessage.vue?vue&type=style&index=0&id=4d2a3ae4&lang=scss&scoped=true&":
+/*!*************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/style-loader!./node_modules/css-loader!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--7-2!./node_modules/sass-loader/dist/cjs.js??ref--7-3!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/sendMessage.vue?vue&type=style&index=0&id=4d2a3ae4&lang=scss&scoped=true& ***!
+  \*************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+var content = __webpack_require__(/*! !../../../node_modules/css-loader!../../../node_modules/vue-loader/lib/loaders/stylePostLoader.js!../../../node_modules/postcss-loader/src??ref--7-2!../../../node_modules/sass-loader/dist/cjs.js??ref--7-3!../../../node_modules/vue-loader/lib??vue-loader-options!./sendMessage.vue?vue&type=style&index=0&id=4d2a3ae4&lang=scss&scoped=true& */ "./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/sendMessage.vue?vue&type=style&index=0&id=4d2a3ae4&lang=scss&scoped=true&");
+
+if(typeof content === 'string') content = [[module.i, content, '']];
+
+var transform;
+var insertInto;
+
+
+
+var options = {"hmr":true}
+
+options.transform = transform
+options.insertInto = undefined;
+
+var update = __webpack_require__(/*! ../../../node_modules/style-loader/lib/addStyles.js */ "./node_modules/style-loader/lib/addStyles.js")(content, options);
+
+if(content.locals) module.exports = content.locals;
+
+if(false) {}
+
+/***/ }),
+
 /***/ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/sendReview.vue?vue&type=style&index=0&id=55b364ea&lang=scss&scoped=true&":
 /*!************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/style-loader!./node_modules/css-loader!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--7-2!./node_modules/sass-loader/dist/cjs.js??ref--7-3!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/sendReview.vue?vue&type=style&index=0&id=55b364ea&lang=scss&scoped=true& ***!
@@ -4844,7 +4932,10 @@ var render = function () {
         ],
         staticClass: "form-control",
         staticStyle: { height: "200px" },
-        attrs: { id: "floatingTextarea2", placeholder: "Scrivi qui il tuo" },
+        attrs: {
+          id: "floatingTextarea2",
+          placeholder: "Scrivi qui il tuo messaggio",
+        },
         domProps: { value: _vm.newMessage.text },
         on: {
           input: function ($event) {
@@ -4867,15 +4958,43 @@ var render = function () {
         staticClass: "btn btn-outline-info my-2",
         on: {
           click: function ($event) {
-            return _vm.postMessage()
+            return _vm.openModal()
           },
         },
       },
-      [_vm._v("Invia")]
+      [_vm._v("\n    Invia\n  ")]
     ),
+    _vm._v(" "),
+    _c("div", { staticClass: "bg-modal" }, [
+      _c("div", { staticClass: "modal-contents" }, [
+        _c("h5", [_vm._v("Il tuo messaggio è stato inviato.")]),
+        _vm._v(" "),
+        _vm._m(0),
+        _vm._v(" "),
+        _c(
+          "button",
+          {
+            staticClass: "close btn btn-outline-info my-2",
+            on: {
+              click: function ($event) {
+                return _vm.closeModal()
+              },
+            },
+          },
+          [_vm._v("Invia un altro messaggio.")]
+        ),
+      ]),
+    ]),
   ])
 }
-var staticRenderFns = []
+var staticRenderFns = [
+  function () {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "content" }, [_c("ul")])
+  },
+]
 render._withStripped = true
 
 
@@ -20852,7 +20971,9 @@ __webpack_require__.r(__webpack_exports__);
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _sendMessage_vue_vue_type_template_id_4d2a3ae4_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sendMessage.vue?vue&type=template&id=4d2a3ae4&scoped=true& */ "./resources/js/components/sendMessage.vue?vue&type=template&id=4d2a3ae4&scoped=true&");
 /* harmony import */ var _sendMessage_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./sendMessage.vue?vue&type=script&lang=js& */ "./resources/js/components/sendMessage.vue?vue&type=script&lang=js&");
-/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* empty/unused harmony star reexport *//* harmony import */ var _sendMessage_vue_vue_type_style_index_0_id_4d2a3ae4_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./sendMessage.vue?vue&type=style&index=0&id=4d2a3ae4&lang=scss&scoped=true& */ "./resources/js/components/sendMessage.vue?vue&type=style&index=0&id=4d2a3ae4&lang=scss&scoped=true&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
 
 
 
@@ -20860,7 +20981,7 @@ __webpack_require__.r(__webpack_exports__);
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__["default"])(
   _sendMessage_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _sendMessage_vue_vue_type_template_id_4d2a3ae4_scoped_true___WEBPACK_IMPORTED_MODULE_0__["render"],
   _sendMessage_vue_vue_type_template_id_4d2a3ae4_scoped_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
@@ -20889,6 +21010,22 @@ component.options.__file = "resources/js/components/sendMessage.vue"
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_sendMessage_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./sendMessage.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/sendMessage.vue?vue&type=script&lang=js&");
 /* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_sendMessage_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/sendMessage.vue?vue&type=style&index=0&id=4d2a3ae4&lang=scss&scoped=true&":
+/*!***********************************************************************************************************!*\
+  !*** ./resources/js/components/sendMessage.vue?vue&type=style&index=0&id=4d2a3ae4&lang=scss&scoped=true& ***!
+  \***********************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_sass_loader_dist_cjs_js_ref_7_3_node_modules_vue_loader_lib_index_js_vue_loader_options_sendMessage_vue_vue_type_style_index_0_id_4d2a3ae4_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/style-loader!../../../node_modules/css-loader!../../../node_modules/vue-loader/lib/loaders/stylePostLoader.js!../../../node_modules/postcss-loader/src??ref--7-2!../../../node_modules/sass-loader/dist/cjs.js??ref--7-3!../../../node_modules/vue-loader/lib??vue-loader-options!./sendMessage.vue?vue&type=style&index=0&id=4d2a3ae4&lang=scss&scoped=true& */ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/sendMessage.vue?vue&type=style&index=0&id=4d2a3ae4&lang=scss&scoped=true&");
+/* harmony import */ var _node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_sass_loader_dist_cjs_js_ref_7_3_node_modules_vue_loader_lib_index_js_vue_loader_options_sendMessage_vue_vue_type_style_index_0_id_4d2a3ae4_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_sass_loader_dist_cjs_js_ref_7_3_node_modules_vue_loader_lib_index_js_vue_loader_options_sendMessage_vue_vue_type_style_index_0_id_4d2a3ae4_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_sass_loader_dist_cjs_js_ref_7_3_node_modules_vue_loader_lib_index_js_vue_loader_options_sendMessage_vue_vue_type_style_index_0_id_4d2a3ae4_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_sass_loader_dist_cjs_js_ref_7_3_node_modules_vue_loader_lib_index_js_vue_loader_options_sendMessage_vue_vue_type_style_index_0_id_4d2a3ae4_lang_scss_scoped_true___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+
 
 /***/ }),
 
@@ -21312,7 +21449,7 @@ var app = new vue__WEBPACK_IMPORTED_MODULE_2___default.a({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/agostinodefelice/Desktop/progetto finale/BDoctor/resources/js/vue.js */"./resources/js/vue.js");
+module.exports = __webpack_require__(/*! C:\Users\ermax\Boolean\BDoctor\resources\js\vue.js */"./resources/js/vue.js");
 
 
 /***/ })
--- resources/js/components/sendMessage.vue
+++ resources/js/components/sendMessage.vue
@@ -1,56 +1,124 @@
 <template>
-   
-    <div class="message">
-        <h4>Contatta il medico per un appuntamento</h4>
+  <div class="message">
+    <h4>Contatta il medico per un appuntamento</h4>
 
-        <div class="form-floating my-1">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Nome" v-model="newMessage.name">
-            <label for="floatingInput">Nome</label>
-        </div>
-
-        <div class="form-floating my-1">
-            <input type="email" class="form-control" id="floatingInput2" placeholder="email" v-model="newMessage.email">
-            <label for="floatingInput2">email</label>
-        </div>
-
-        <div class="form-floating my-1">
-            <textarea class="form-control" id="floatingTextarea2" style="height: 200px" placeholder="Scrivi qui il tuo" v-model="newMessage.text"></textarea>
-            <label for="floatingTextarea2">Scrivi qui il tuo messaggio</label>
-        </div>
-
-        <button class="btn btn-outline-info my-2" @click="postMessage()">Invia</button>
-
+    <div class="form-floating my-1">
+      <input
+        type="text"
+        class="form-control"
+        id="floatingInput"
+        placeholder="Nome"
+        v-model="newMessage.name"
+      />
+      <label for="floatingInput">Nome</label>
     </div>
+
+    <div class="form-floating my-1">
+      <input
+        type="email"
+        class="form-control"
+        id="floatingInput2"
+        placeholder="email"
+        v-model="newMessage.email"
+      />
+      <label for="floatingInput2">email</label>
+    </div>
+
+    <div class="form-floating my-1">
+      <textarea
+        class="form-control"
+        id="floatingTextarea2"
+        style="height: 200px"
+        placeholder="Scrivi qui il tuo messaggio"
+        v-model="newMessage.text"
+      ></textarea>
+      <label for="floatingTextarea2">Scrivi qui il tuo messaggio</label>
+    </div>
+
+    <button class="btn btn-outline-info my-2" @click="openModal()">
+      Invia
+    </button>
+
+    <div class="bg-modal">
+      <div class="modal-contents">
+        <h5>Il tuo messaggio è stato inviato.</h5>
+        <div class="content">
+            <ul>
+                <!-- bug: appena inserisci un valore di un v-model il container scompare dal flusso -->
+         <!--   <li>Nome: {{$newMessage.name}}</li> 
+                <li>Email: {{$newMessage.text}}</li> 
+                <li>Testo Messaggio: {{$newMessage.email}}</li> -->
+            </ul>
+        </div>
+        <button class="close btn btn-outline-info my-2" @click="closeModal()">Invia un altro messaggio.</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    props: {
-        doctor_id: Number,
+  props: {
+    doctor_id: Number,
+  },
+  data() {
+    return {
+      newMessage: {
+        name: "",
+        email: "",
+        text: "",
+        doctor_id: "",
+      },
+    };
+  }, // end of data
+  methods: {
+    postMessage: async function () {
+      this.newMessage.doctor_id = this.doctor_id;
+      await axios.post("api/message", this.newMessage);
     },
-    data(){
-        return {
-            newMessage: {
-                name: '',
-                email:'',
-                text: '',
-                doctor_id: '',
-            }
-        }
-    }, // end of data
-    methods: {
-        postMessage: async function() {
-            this.newMessage.doctor_id = this.doctor_id;
-            await axios.post('api/message', this.newMessage);
+    openModal() {
+      document.querySelector(".bg-modal").style.display = "flex";
+    },
 
-        },
-    }
-
-}
+    closeModal() {
+      document.querySelector(".bg-modal").style.display = "none";
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.message{
+    position: relative;
+}
+ .bg-modal {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      display: none;
+      justify-content: center;
+      align-items: center;
+    }
     
+    .modal-contents {
+      height: 100%;
+      width: 500px;
+      background-color: white;
+      text-align: center;
+      padding: 20px;
+      position: relative;
+      border-radius: 4px;
+      background-color: #3A6EA5;
+      overflow: auto;
+    }
+    
+    .close {
+        width: 190px;
+        margin: 50px auto;
+        bottom: 0;
+        font-size: 14px;
+    }
 </style>
